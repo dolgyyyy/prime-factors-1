1bf8b1f46cbca0f242adc27e5ae58a0810df2d90
heehoha <dolgyuk@gmail.com>
2024-06-13 09:44:31 +0900

    [feature] page 21

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
+from prime_factors import PrimeFactor
+
 
 class TestPrimeFactor(TestCase):
     def test_prime_factor_of_1(self):
-        pass
+        prime_factor = PrimeFactor()
+        self.assertEqual([], prime_factor.of(1))
